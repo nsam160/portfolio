aaf2cf963ef771eccfe5fcabdd3a84d56f20ab20
nsam160 <nvsam@ucsd.edu>
2025-04-24 16:34:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
-const projects = await fetchJSON('./lib/projects.json');
+const projects = await fetchJSON('lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.latest-projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
